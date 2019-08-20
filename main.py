1140109e499093bb9da4a7d185b7f23e7bfa8478
svoke <49045881+svoke@users.noreply.github.com>
2019-08-20 22:00:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ parser.add_option("--website", dest="website",help="choose a website")
 (options, args) = parser.parse_args()
 
 
-CHROME_DVR_DIR = 'C:\webdrivers\chromedriver.exe'
+CHROME_DVR_DIR = 'C:\Users\svoke\webdrivers\chromedriver.exe'
 
 def wizard():
     print (banner)
